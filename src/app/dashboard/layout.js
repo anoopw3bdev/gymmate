84b1eded200f0b3fee3192d87c7ca0442b7f9547
anoop <anoopano20@gmail.com>
2024-01-12 16:42:32 +0530

    initial setup

---
+++ src/app/dashboard/layout.js
@@ -0,0 +1,10 @@
+import Nav from "../components/Nav"
+
+export default function DashboardLayout({ children }) {
+    return(
+        <section>
+            <Nav/>
+            {children}
+        </section>
+    )
+}
---
+++ src/app/dashboard/members/page.js
@@ -0,0 +1,8 @@
+
+const Page = () => {
+  return (
+    <div>Member page</div>
+  )
+}
+
+export default Page

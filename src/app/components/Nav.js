038c9b8152641c660aaee0edb8afc1fdb8484f22
anoop <anoopano20@gmail.com>
2024-01-30 14:10:25 +0530

    some styling

--- src/app/components/Nav.js
+++ src/app/components/Nav.js
@@ -8,8 +8,10 @@ const Nav = () => {
         className="flex items-center gap-x-2 font-medium text-gray-500 hover:text-gray-700 md:border-s md:border-gray-300 md:my-6 md:ps-6 ml-auto pr-10" 
         href="#"
       >
-        <Icons name="userIcon"/>
         Log out
+        <div className="w-10 h-10 bg-gradient-to-r from-red-300 via-red-400 to-red-600 text-white rounded-3xl flex items-center justify-center">
+          <Icons name="userIcon"/>
+        </div>
       </Link>
     </header>
   )
